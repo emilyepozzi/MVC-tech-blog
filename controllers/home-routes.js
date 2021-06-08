2d78813d239f0588d83b93ffcfab2520ddf26b2a
Emily Pozzi <epozzi5@yahoo.com>
2021-06-07 21:17:06 -0500

    js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,40 +1,108 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
-router.use(function(req, res, next) {
-  console.log('Something is happening.');
-  next();
-});
-
-router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our rest video api!' });  
-});
-
-
-router.route('/videos')
-
-  .post(function(req, res) {
-
-    var video = new Video();
-    video.title = req.body.title;
-
-    video.save(function(err) {
-  if (err)
-    res.send(err);
-
-  res.json({ message: 'Video criado!' });
-});
-
-
-  })
-
-  .get(function(req, res) {
-    Video.find(function(err, videos) {
-      if (err)
-        res.send(err);
-
-      res.json(videos);
-    });
+router.get('/', (req, res) => {
+    console.log(req.session);
+    
+    Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'created_at',
+        'post_content'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username', 'twitter', 'github']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username', 'twitter', 'github']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   });
 
-module.exports.router = router;
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
+  router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('signup');
+  });
+
+  router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'title',
+        'created_at',
+        'post_content'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username', 'twitter', 'github']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username', 'twitter', 'github']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+  
+        // pass data to template
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+module.exports = router;

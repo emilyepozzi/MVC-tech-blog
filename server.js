7d82e9fed25d8732ed77dc42e789b445d273fb73
Emily Pozzi <epozzi5@yahoo.com>
2021-06-18 20:18:55 -0500

    refactored server.js

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-const express = require('express');
-const routes =require('./controllers');
-const sequelize = require('./config/connection')
 const path = require('path');
-
-const helpers = require('./utils/helpers');
-
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
-
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const routes =require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -31,12 +27,14 @@ const sess = {
 
 app.use(session(sess));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 

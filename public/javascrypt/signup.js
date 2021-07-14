d4594759287c40dbed6d0ce51cd0a946bb3e3e4f
Emily Pozzi <epozzi5@yahoo.com>
2021-07-14 15:24:07 -0500

    handlebars

--- public/javascrypt/signup.js
+++ public/javascrypt/signup.js
@@ -1,33 +1,52 @@
 async function signupFormHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username-signup').nodeValue.trim();
-    const email = document.querySelector('#email-signup').nodeValue.trim();
-    const password = document.querySelector('#password-signup').nodeValue.trim();
-    const twitter = document.querySelector('#twitter-signup').nodeValue.trim();
-    const github = document.querySelector('#github-signup').nodeValue.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const github = document.querySelector('#github-signup').value.trim();
+    
+    console.log(username)
+    console.log(email)
+    console.log(password)
+    console.log(github)
 
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                twitter,
-                github,
-                password
-            }),
-            headers: { 'Content-Type': 'application.json' }
-        });
+    fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: username,
+            email: email,
+            github: github,
+            password: password
+        }),
+        headers: { 'Content-Type': 'application.json', 'Accept': 'application/json' }
+    })
+    .then(function(){
+        console.log("success")
+    })
+    .catch(err => console.log(err))
 
-        //response
-        if (response.ok) {
-            console.log('success');
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
-    }
+    // if (username && email && password) {
+    //     const response = await fetch('/api/users', {
+    //         method: 'POST',
+    //         headers: { 'Content-Type': 'application.json', 'Accept': 'application/json' },
+    //         body: JSON.stringify({
+    //             username: "username",
+    //             email: email,
+    //             github: github,
+    //             password: password
+    //         })
+            
+    //     });
+
+    //     //response
+    //     if (response.ok) {
+    //         console.log('success');
+    //         document.location.replace('/dashboard');
+    //     } else {
+    //         alert(response.statusText);
+    //     }
+    // }
 }
 
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
